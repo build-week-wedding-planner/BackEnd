7f10ac975513bf308e8fb90dd1e87403cf5b0501
amberm <nekosenpai@gmail.com>
2019-05-28 11:51:33 -0600

    starter server in place

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+require('dotenv').config();
+
+//const configureRoutes = require('../config/routes.js');
+
+const server = express();
+
+const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+server.use(express.json());
+
+server.get('/', (req,res) => {
+    res.send("Avengers Assemble !!!")
+} )
+
+configureRoutes(server);
+
+module.exports = server;
