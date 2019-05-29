bd9638df519fae07581dda4ecaf68c34ee1bcd75
amberm <nekosenpai@gmail.com>
2019-05-29 14:13:50 -0600

    changed env variable

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require('knex');
 
 const knexConfigy = require('../knexfile')
 
-const dbEngine = process.env.DB || 'production';
+const dbEngine = process.env.NODE_ENV || 'development';
 
 module.exports = knex(knexConfigy[dbEngine]);
